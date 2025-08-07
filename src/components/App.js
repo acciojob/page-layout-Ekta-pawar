fcc20131cc2979a0dc484afbf2642a435c87441c
Ekta <ektapawar0001@gmail.com>
2025-08-08 00:07:41 +0530

    header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 
 import React from "react";
-import './../styles/App.css';
+import PageLayout from "./Pagelayout";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my website</h1>
-      <p>This is the content of my side</p>
-      <p>2023 My Website.All rights reserved</p>
+    <PageLayout
+      header={<h1>This is the Header</h1>}
+      footer={<p>This is the Footer</p>}
+    >
+      <p>This is the main content (children)</p>
+    </PageLayout>
+
     </div>
   )
 }
---
+++ src/components/Pagelayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../styles/App.css';
+const PageLayout = ({ header, footer, children }) => {
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  );
+};
+
+export default PageLayout;
